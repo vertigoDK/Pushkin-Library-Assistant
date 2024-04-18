97376584dc9f0a6c7d63365ba80a54bdc471b81b
Denis Kushnarev <rrrroor456@gmail.com>
2024-04-18 18:12:38 +0500

    Добавлен config.py

---
+++ app/appConfig.py
@@ -0,0 +1,3 @@
+DATA_FILE_PATH = "../DataBase/txtdb/db.txt" #Путь к базе знаний
+CHROMA_DB_PATH = "../DataBase/chromadb" #Путь к базе данных хрома
+CHROMA_DB_NAME = "chroma" #Название базы данных chroma
