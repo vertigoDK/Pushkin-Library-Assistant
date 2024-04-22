5ca14101c0b35dc4bb90624901752968238a41e6
Denis Kushnarev <rrrroor456@gmail.com>
2024-04-22 12:58:26 +0500

    добавил приложение core и создал админа

--- PushkinLibraryBackEnd/settings.py
+++ PushkinLibraryBackEnd/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
