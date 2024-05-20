62a58272bee982801a31d50733e90c8452f2a026
Riffaells <riffaells@gmail.com>
2024-05-20 17:27:57 +0500

    промежуточный комит

--- PushkinLibraryBackEnd/settings.py
+++ PushkinLibraryBackEnd/settings.py
@@ -1,4 +1,4 @@
-
+from django.forms import CharField
 from django.utils.translation import gettext_lazy as _
 from pathlib import Path
 import os
@@ -112,30 +112,32 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
 
 LOCALE_PATHS = (
    os.path.join(BASE_DIR, 'conf/locale'),
-   # os.path.join(BASE_DIR, 'polls/locale')
 )
 
 LANGUAGES = [
     ("en", _("English")),
     ("ru", _("Russian")),
-    ("kz", _("Kazak")),
+    ("kz", _("Kazakh")),
 ]
 MULTILINGUAL_LANGUAGES = (
     "en-us",
     "kz",
-    "ru",
+    "ru-ru",
 )
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
