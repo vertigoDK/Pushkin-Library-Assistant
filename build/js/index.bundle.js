98bace223bb1e012d0a61f139ab451a0fd18d3a1
Adikkkkkkkk <raximov_adil@mail.ru>
2024-06-04 14:00:47 +0500

    created header navigation

---
+++ build/js/index.bundle.js
@@ -0,0 +1,1 @@
+!function(){"use strict";!function(){const o=document.querySelector(".mobile-nav-btn"),e=document.querySelector(".mobile-nav"),n=document.querySelector(".nav-icon");o.onclick=function(){e.classList.toggle("mobile-nav--open"),n.classList.toggle("nav-icon--active"),document.body.classList.toggle("no-scroll")}}()}();
