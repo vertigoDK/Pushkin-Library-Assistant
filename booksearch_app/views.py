65a23d6dbdf90475c8290992f02414294c0e457a
Riffaells <riffaells@gmail.com>
2024-06-11 15:22:39 +0500

    обновление

--- booksearch_app/views.py
+++ booksearch_app/views.py
@@ -2,12 +2,14 @@ import os
 
 from django.http import JsonResponse
 from django.shortcuts import render
+from difflib import SequenceMatcher
+from Levenshtein import ratio, distance
 
 
 def parse_data_file():
     file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'database', 'data.txt')
     books = []
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, 'r', encoding='utf-8', errors="ignore") as file:
         for line in file:
             title = line.strip()  # Получаем название книги из строки без лишних пробелов
             if title:  # Проверяем, что строка не пустая
@@ -15,20 +17,43 @@ def parse_data_file():
     return books
 
 
+def similar(a: str, b: str):
+    return 1 - (SequenceMatcher(None, b, a).find_longest_match().size / len(a))
+
+
 def search_books(request):
     query = request.GET.get('q', '').lower()
     results = []
+    sort_index = []
     books = parse_data_file()
 
     if query:
         for book in books:
-            if query in book.get('title', '').lower():
+            if x := ratio(book.get('title', '').lower(), query) > 0.7:
                 results.append({'title': book['title']})
-                if len(results) >= 20:
-                    break
+                sort_index.append(x)
+            if len(results) >= 20:
+                break
 
     return JsonResponse({'results': results})
 
 
 def search_page(request):
     return render(request, 'booksearch_app/search_book.html', context={'show_chatbot': True})
+
+
+def levenshtein_distance(a: str, b: str):
+    if len(b) == 0:
+        return len(a)
+
+    previous_row = range(len(b) + 1)
+    for i, c1 in enumerate(a):
+        row = [i + 1]
+        for j, c2 in enumerate(b):
+            insertions = previous_row[j + 1] + 1
+            deletions = row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            row.append(min(insertions, deletions, substitutions))
+        previous_row = row
+
+    return previous_row[-1]
--- database/converter.py
+++ database/converter.py
@@ -17,122 +17,138 @@ class KeyL(Enum):
         return str(self)
 
 
-none = "Null"
-lst_keyL = list(KeyL)
+class Converter:
+    none = "Null"
+    lst_keyL = list(KeyL)
 
+    symbols = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "\"": "&quot;",
+        "'": "&apos;"
+    }
 
-def get_format_txt(s):
-    result = []
-    for j in s:
+    def __init__(self, type_file: str = 'txt', file_name: str = "output", file_input: str = "booktest.xml", limit_book: int = -1):
+        self.type_file = type_file
+        if file_name.find(".") == -1:
+            self.file_name = f'{file_name}.{type_file}'
+        else:
+            self.file_name = file_name
+        self.file_input = file_input
+        self.limit_book = limit_book
 
-        for i in lst_keyL:
-            result.append(f"{i}: {j.get(i, none)}")
-    return result
-
-
-def get_format_json(s):
-    result = []
-
-    for i in lst_keyL:
-        result.append(f'\t\t"{i}": "{s.get(i, none)}"')
-    return '\t{\n' + ',\n'.join(result) + '\n\t}'
-
-
-def get_dict(s):
-    result = {}
-
-    if s.get(KeyL.title, none) == none:
-        return ""
-
-    for i in lst_keyL:
-        result[i] = s.get(i, none)
-    return result
-
-
-def get_text(s, *r) -> str:
-    result = s.replace("\n", "")
-    for i in r:
-        result = result.replace(i, "")
-    return result
-
-
-def read_irbis(path: str, limit_book: int):
-    print(path)
-    result = []
-    with open(path, mode='r+', encoding='utf8', errors="ignore") as file_read:
-        subfield = dict()
-        tag = -1
-        count_book = 0
-        for i in file_read.readlines():
-            content = " ".join(i.strip("\n").strip().split(' '))
-            if content.startswith("<field"):
-                tag = int("".join(
-                    i if i.isdigit() else "" for i in list(content[content.index("<"): content.index(">")])))
-            elif content.startswith("</field"):
-                tag = -1
-            elif content.startswith("</record>"):
-                count_book += 1
-                if count_book > limit_book > 0:
-                    break
-                print(f"Книга #{count_book}")
-                result.append(get_dict(subfield))
-                subfield = dict()
-            else:
-
-                if 102 == tag:
-                    subfield[KeyL.lang] = get_text(content, '<field tag="102">')
-
-                if 101 == tag:
-                    subfield[KeyL.country] = get_text(content, '<field tag="101">')
-                if 200 == tag:
-                    if 'code="A"' in content:
-                        title = get_text(content, '<subfield code="A">', '</subfield>')
-                        subfield[KeyL.title] = title
-                    elif 'code="F"' in content:
-                        author = get_text(content, '<subfield code="F">', '</subfield>')
-                        subfield[KeyL.author] = author
-                if 331 == tag:
-                    subfield[KeyL.description] = content.replace('<field tag="101">', "").replace('\n', '')
-                if 610 == tag and not content.startswith("<field"):
-                    subfield[KeyL.keywords] = subfield.get(KeyL.keywords, "") + get_text(content,
-                                                                                         '<field tag="610">') + ", "
-
-        print(len(result))
+    def convert_format_txt(self, s):
+        result = []
+        for j in s:
+            for i in self.lst_keyL:
+                result.append(f"{i}: {j.get(i, self.none)}")
         return result
 
+    def convert_format_json(self, s):
+        result = []
+        for i in self.lst_keyL:
+            result.append(f'\t\t"{i}": "{s.get(i, self.none)}"')
+        return '\t{\n' + ',\n'.join(result) + '\n\t}'
 
-def export_txt(file_output: str, file_input: str, limit_book):
-    data = read_irbis(file_input, limit_book)
-    write_text = []
-    for i in data:
-        if i:
-            write_text.append(f'{",".join(get_format_txt(data))}')
-    with open(file_output, 'w+', encoding='utf8', errors="ignore") as file_write:
-        file_write.write('\n'.join(write_text))
+    def get_dict(self, s):
+        result = {}
 
+        # if s.get(KeyL.title, self.none) == self.none:
+        #     return ""
 
-def export_json(file_output: str, file_input: str, limit_book):
-    data = read_irbis(file_input, limit_book)
-    result = []
-    for i in data:
-        if i:
-            result.append(f'{get_format_json(i)}')
-    write_text = "[\n" + ',\n'.join(result) + '\n]'
-    with open(file_output, 'w+', encoding='utf8', errors="ignore") as file_write:
-        file_write.write(write_text)
+        for i in self.lst_keyL:
+            result[i] = self.replace_symbols(s.get(i, self.none))
+        return result
+
+    def replace_symbols(self, text:str):
+        for i, k in self.symbols.items():
+            text = text.replace(i, k)
+        return text
+
+    @staticmethod
+    def get_text(s, *r) -> str:
+        result = s.replace("\n", "")
+        for i in r:
+            result = result.replace(i, "")
+        return result
+
+    def read_irbis(self):
+        result = []
+        with open(self.file_input, mode='r+', encoding='utf8', errors="ignore") as file_read:
+            subfield = dict()
+            tag = -1
+            count_book = 0
+            for i in file_read.readlines():
+                content = " ".join(i.strip("\n").strip().split(' '))
+                if content.startswith("<field"):
+                    tag = int("".join(
+                        i if i.isdigit() else "" for i in list(content[content.index("<"): content.index(">")])))
+                elif content.startswith("</field"):
+                    tag = -1
+                elif content.startswith("</record>"):
+                    count_book += 1
+                    if count_book > self.limit_book > 0:
+                        break
+                    print(f"Книга #{count_book}")
+                    result.append(self.get_dict(subfield))
+                    subfield = dict()
+                else:
+
+                    if 102 == tag:
+                        subfield[KeyL.lang] = self.get_text(content, '<field tag="102">')
+
+                    if 101 == tag:
+                        subfield[KeyL.country] = self.get_text(content, '<field tag="101">')
+                    if 200 == tag:
+                        if 'code="A"' in content:
+                            title = self.get_text(content, '<subfield code="A">', '</subfield>')
+                            subfield[KeyL.title] = title
+                        elif 'code="F"' in content:
+                            author = self.get_text(content, '<subfield code="F">', '</subfield>')
+                            subfield[KeyL.author] = author
+                    if 331 == tag:
+                        subfield[KeyL.description] = content.replace('<field tag="101">', "").replace('\n', '')
+                    if 610 == tag and not content.startswith("<field"):
+                        subfield[KeyL.keywords] = subfield.get(KeyL.keywords, "") + self.get_text(content,
+                                                                                             '<field tag="610">') + ", "
+
+            print(f"Всего книг:{len(result)}")
+            return result
+
+    def export_txt(self):
+        data = self.read_irbis()
+        write_text = []
+        for i in data:
+            if i:
+                write_text.append(f'{",".join(self.convert_format_txt(data))}')
+        self.save('\n'.join(write_text))
+
+    def export_json(self,):
+        data = self.read_irbis()
+        result = []
+        for i in data:
+            if i:
+                result.append(f'{self.convert_format_json(i)}')
+        write_text = "[\n" + ',\n'.join(result) + '\n]'
+        self.save(write_text)
+
+    def save(self, write_text:str):
+        with open(self.file_name, 'w+', encoding='utf8', errors="ignore") as file_write:
+            file_write.write(write_text)
+
 
 
 def start_settings():
-    type_file = input('Введите тип файла: json, txt (default: json) >> ')
-    if type_file != 'txt':
-        type_file = 'json'
+    type_file = input('Введите тип файла: json, txt (default: txt) >> ')
+    if type_file != 'json':
+        type_file = 'txt'
     file_name = input(f'Введите название файла (default: output_books.{type_file}) >> ')
     if not file_name:
         file_name = f'output_books.{type_file}'
     file_input = input('Введите название файла (default: booktest.xml) >> ')
     if not file_input:
         file_input = 'booktest.xml'
-
     limit_book = input('Ограничить число книг? (default: -1 (нет)) >> ')
     if not limit_book:
         limit_book = -1
@@ -144,7 +160,6 @@ def start_settings():
 
 if __name__ == '__main__':
     type_file, output_file, input_file, limit_book = start_settings()
-    if type_file == 'json':
-        export_json(output_file, input_file, limit_book)
-    else:
-        export_txt(output_file, input_file, limit_book)
+
+    c = Converter(type_file, output_file, input_file, limit_book)
+    c.export_txt()
