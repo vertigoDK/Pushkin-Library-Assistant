baa63ff444e2fd7d63d2cbae4e073ec32b9c59aa
Riffaells <riffaells@gmail.com>
2024-06-22 23:33:39 +0500

    время

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.timezone import now
 
 from app.news_app.models import News
 
 
 def index(request):
     news = News.objects.all().order_by('-pub_date')
-    return render(request=request, template_name='core.html', context={'show_chatbot': True, 'news': news})
+    return render(request=request, template_name='core.html',
+                  context={'show_chatbot': True, 'news': news, 'server_time': now(), })
