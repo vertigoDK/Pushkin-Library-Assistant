c7fd470111eb6402e84b3b7f0730d131dde04c74
Riffaells <riffaells@gmail.com>
2024-06-23 23:57:08 +0500

    добавление читателей

--- app/services/readers/views.py
+++ app/services/readers/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    return render(request=request, template_name='readers.html', context={'show_chatbot': True})
+
+
+
 def renew_the_book(request):
     return render(request=request, template_name='renew-the-book.html', context={'show_chatbot': True})
