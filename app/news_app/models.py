ad5d648dc31d87c1e061ece0a777d699555f7766
Your Name <you@example.com>
2024-06-23 16:45:52 +0300

    разнгые языки

--- app/news_app/models.py
+++ app/news_app/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from autoslug import AutoSlugField
 from django.utils.translation import gettext as _
 
-
 class News(models.Model):
     title = models.CharField(max_length=255, verbose_name=_('title'))
     slug = AutoSlugField(populate_from='title', unique=True)
-    content = models.TextField(verbose_name='Содержание')
+    content_ru = models.TextField(verbose_name='Содержание (рус)', blank=True, null=True)
+    content_kz = models.TextField(verbose_name='Содержание (каз)', blank=True, null=True)
+    content_eng = models.TextField(verbose_name='Содержание (анг)', blank=True, null=True)
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     main_image = models.ImageField(upload_to='news_images/', blank=True, null=True, verbose_name='Основное изображение')
     contact_info = models.CharField(max_length=100, blank=True, null=True, verbose_name='Контактная информация')
