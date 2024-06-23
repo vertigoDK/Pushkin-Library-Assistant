c949f43224aaf6b96bddfc8b6d836d51ac8932fb
Your Name <you@example.com>
2024-06-23 18:48:52 +0300

    tet

--- app/chatbot_app/urls.py
+++ app/chatbot_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mini', views.index, name='chat_bot'),
+    path('mini/', views.index, name='chat_bot'),
     path('', views.chat_page, name='chat_page'),
 ]
