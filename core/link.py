57a84c1bbe842b4e7063b02991d83e4322ca2119
Riffaells <riffaells@gmail.com>
2024-06-25 18:50:52 +0500

    header and link

--- core/link.py
+++ core/link.py
@@ -1,10 +1,10 @@
-EXTERNAL_LINKS = {
-    ('2Gis', 'https://go.2gis.com/kt5nh'),
+EXTERNAL_LINKS = (
+    ('Telegram', 'https://t.me/bibliotekapushkina'),
     ('Twitter', 'https://twitter.com/Pushkinlibrary'),
+    ('Vk', 'https://m.vk.com/public195321477?from=group'),
     ('Whatsapp', 'https://web.whatsapp.com/send?phone=+77052821127'),
     ('Instagram', 'https://www.instagram.com/pushkinlibrary/'),
-    ('Facebook', 'https://www.facebook.com/groups/1522352141410218/'),
     ('Youtube', 'https://www.youtube.com/user/VKOBP'),
-    ('Vk', 'https://m.vk.com/public195321477?from=group'),
-    ('Telegram', 'https://t.me/bibliotekapushkina'),
-}
+    ('Facebook', 'https://www.facebook.com/groups/1522352141410218/'),
+    ('2Gis', 'https://go.2gis.com/kt5nh'),
+)
