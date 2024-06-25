8bd359ab3d73ecdc67c230d36b56e7e1bffc32df
Riffaells <riffaells@gmail.com>
2024-06-25 17:19:59 +0500

    добавление футера

--- app/chatbot_app/urls.py
+++ app/chatbot_app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mini/', views.index, name='chat_bot'),
     path('', views.chat_page, name='chat_page'),
 ]
--- app/chatbot_app/views.py
+++ app/chatbot_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request=request, template_name='chat.html', context={'show_chatbot': False})
-
-
 def chat_page(request):
     return render(request=request, template_name='chat_page.html', context={'show_chatbot': False, 'hide_footer': True})
