42396df9a89ca0cfb99cc18d7570af7ee837a959
Riffaells <riffaells@gmail.com>
2024-06-26 10:43:53 +0500

    исправление

--- app/news_app/models.py
+++ app/news_app/models.py
@@ -4,16 +4,16 @@ from django.utils.translation import gettext as _
 
 
 class News(models.Model):
-    title_ru = models.CharField(max_length=255, verbose_name=_('titleRu'))
-    title_kz = models.CharField(max_length=255, verbose_name=_('titleKz'))
-    title_en = models.CharField(max_length=255, verbose_name=_('titleEn'))
-    slug = AutoSlugField(populate_from='title', unique=True)
-    content_ru = models.TextField(verbose_name='Содержание (рус)', blank=True, null=True)
-    content_kz = models.TextField(verbose_name='Содержание (каз)', blank=True, null=True)
-    content_eng = models.TextField(verbose_name='Содержание (анг)', blank=True, null=True)
+    title_ru = models.CharField(max_length=255, verbose_name=_('titleRu'), default='')
+    title_kz = models.CharField(max_length=255, verbose_name=_('titleKz'), default='')
+    title_en = models.CharField(max_length=255, verbose_name=_('titleEn'), default='')
+    slug = AutoSlugField(populate_from='title', unique=True, default=None)
+    content_ru = models.TextField(verbose_name='Содержание (рус)', blank=True, null=True, default='')
+    content_kz = models.TextField(verbose_name='Содержание (каз)', blank=True, null=True, default='')
+    content_eng = models.TextField(verbose_name='Содержание (анг)', blank=True, null=True, default='')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
-    main_image = models.ImageField(upload_to='news_images/', blank=True, null=True, verbose_name='Основное изображение')
-    contact_info = models.CharField(max_length=100, blank=True, null=True, verbose_name='Контактная информация')
+    main_image = models.ImageField(upload_to='news_images/', blank=True, null=True, verbose_name='Основное изображение', default='')
+    contact_info = models.CharField(max_length=100, blank=True, null=True, verbose_name='Контактная информация', default='')
     is_archive = models.BooleanField(default=False)
 
     class Meta:
@@ -26,8 +26,8 @@ class News(models.Model):
 
 
 class NewsImage(models.Model):
-    news = models.ForeignKey(News, related_name='additional_images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='news_images/', verbose_name='Дополнительное изображение')
+    news = models.ForeignKey(News, related_name='additional_images', on_delete=models.CASCADE, default='')
+    image = models.ImageField(upload_to='news_images/', verbose_name='Дополнительное изображение', default=None)
 
     class Meta:
         verbose_name = 'Дополнительное изображение'
