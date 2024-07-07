a7c06e2ec710ea0f46d23132dd285f72ad151229
Adikkkkkkkk <raximov_adil@mail.ru>
2024-07-07 12:10:28 +0500

    Created news page and templates pages

--- src/js/news.js
+++ src/js/news.js
@@ -1,3 +1,7 @@
+// Swiper
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
 const cardsText = document.querySelectorAll('.news__card-text');
 
 cardsText.forEach(cardText => {
@@ -5,4 +9,23 @@ cardsText.forEach(cardText => {
 		const newText = cardText.textContent.slice(0, 356);
 		cardText.textContent = `${newText}...`;
 	}
+});
+
+// document.body.style.backgroundColor = 'black';
+
+// Swiper for photo on each page
+
+const swiper = new Swiper('.newsletter__swiper', {
+	spaceBetween: 5,
+	slidesPerView: 4,
+	// freeMode: true,
+	// watchSlidesProgress: true,
+});
+const swiper2 = new Swiper('.newsletter__swiper-2', {
+	spaceBetween: 8,
+	effect: 'fade',
+	speed: 800,
+	thumbs: {
+		swiper: swiper,
+	},
 });
