89e9ddb6bde524d65d08c1a459055fe9497ef700
Adikkkkkkkk <raximov_adil@mail.ru>
2024-07-10 17:50:19 +0500

    created script for tour

--- src/js/modules/tour.js
+++ src/js/modules/tour.js
@@ -1,22 +1,70 @@
+'use strict';
 export default function tourLocationChanges() {
 	const locations = document.querySelectorAll('.tour__location');
 	const images = document.querySelectorAll('.tour__image-picture');
+	// Arrows
+	const btnPrev = document.querySelector('.tour__list-arrow-prev');
+	const btnNext = document.querySelector('.tour__list-arrow-next');
+
+	let active = locations[0];
+
+	function changeImages() {
+		for (let i = 0; i < images.length; i++) {
+			images[i].src = `./img/tour/${active.id}-${i + 1}.jpg`;
+		}
+	}
 
 	locations.forEach(location => {
 		location.addEventListener('click', function () {
-			location.classList.add('active');
+			active = location;
+			active.classList.add('active');
 
-			for (let i = 0; i < images.length; i++) {
-				images[i].src = `./img/tour/${this.id}-${i + 1}.jpg`;
-			}
+			// for (let i = 0; i < images.length; i++) {
+			// 	images[i].src = `./img/tour/${active.id}-${i + 1}.jpg`;
+			// }
+			changeImages();
 
 			// delete classes on other elements
 			locations.forEach(locationInner => {
-				if (locationInner !== this) {
+				if (locationInner !== active) {
 					locationInner.classList.remove('active');
 				}
 			});
 			// delete classes on other elements
 		});
 	});
+	// PREV ARROW CLICKED
+	btnPrev.addEventListener('click', function () {
+		for (let i = 0; i < locations.length; i++) {
+			if (i !== 0 && locations[i] === active) {
+				active.classList.remove('active');
+				active = locations[i - 1];
+				active.classList.add('active');
+			} else if (i === 0 && locations[i] === active) {
+				active.classList.remove('active');
+				active = locations[locations.length - 1];
+				active.classList.add('active');
+				break;
+			}
+		}
+		changeImages();
+	});
+	// PREV ARROW CLICKED
+	// NEXT ARROW CLICKED
+	btnNext.addEventListener('click', function () {
+		for (let i = 0; i < locations.length; i++) {
+			if (i !== locations.length - 1 && locations[i] === active) {
+				active.classList.remove('active');
+				active = locations[i + 1];
+				active.classList.add('active');
+				break;
+			} else if (i === locations.length - 1 && locations[i] === active) {
+				active.classList.remove('active');
+				active = locations[0];
+				active.classList.add('active');
+			}
+		}
+		changeImages();
+	});
+	// NEXT ARROW CLICKED
 }
