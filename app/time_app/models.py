dd642daed35adcf85fe6af83f3969b8fdfbe3e3f
Riffaells <riffaells@gmail.com>
2024-07-16 23:46:05 +0500

    рабочия версия

--- app/time_app/models.py
+++ app/time_app/models.py
@@ -13,7 +13,7 @@ class TimeModel(models.Model):
     date_change = models.DateField(verbose_name='Дата', default=get_future_date)
     is_holiday = models.BooleanField(default=False, verbose_name='Выходной')
     custom_time_start = models.TimeField(verbose_name='Измененное время начала работы', blank=True,
-                                         default=datetime.time(9, 0))
+                                         default=datetime.time(8, 0))
     custom_time_end = models.TimeField(verbose_name='Измененное время окончания работы', blank=True,
                                        default=datetime.time(19, 0))
 
@@ -21,13 +21,6 @@ class TimeModel(models.Model):
         verbose_name = "Изменение Времени"
         verbose_name_plural = "Список Изменений Времени"
 
-    def save(self, *args, **kwargs):
-        if self.is_holiday:
-            self.time_start = None
-            self.time_end = None
-
-        super().save(*args, **kwargs)
-
     def __str__(self):
-        f = '%dsH:%M'
-        return f"{self.date_change} {'- Выходной' if self.is_holiday else f'{self.time_start.strftime(f)} - {self.time_end.strftime(f)}'}"
+        f = 'HH:MM'
+        return f"{self.date_change} {'- Выходной' if self.is_holiday else f'{self.custom_time_start.strftime(f)} - {self.custom_time_end.strftime(f)}'}"
--- core/views.py
+++ core/views.py
@@ -1,17 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.utils.timezone import now
-
+from django.utils.timezone import now, timedelta
 from app.news_app.models import News
 from app.events_app.models import Event
 from app.courses_app.models import Course
+from app.time_app.models import TimeModel
 from .link import EXTERNAL_LINKS
+
+
+def get_next_working_day(date, weekend_days):
+    next_day = date + timedelta(days=1)
+    while next_day.weekday() in weekend_days or TimeModel.objects.filter(date_change=next_day,
+                                                                         is_holiday=True).exists():
+        next_day += timedelta(days=1)
+    return next_day
 
 
 def index(request):
     news = News.objects.all().order_by('-pub_date')
     courses = Course.objects.all()
     events = Event.objects.all()
+
+    current_date = now().date()
+    weekend_days = [0, 1]  # 0 - воскресенье, 1 - понедельник
+
+    try:
+        time_model_today = TimeModel.objects.get(date_change=current_date)
+    except TimeModel.DoesNotExist:
+        time_model_today = None
+
+    next_working_day = get_next_working_day(current_date, weekend_days)
+
+    try:
+        time_model_next = TimeModel.objects.get(date_change=next_working_day)
+    except TimeModel.DoesNotExist:
+        time_model_next = None
+
     context = {
         'show_chatbot': True,
         'news': news,
@@ -19,5 +42,8 @@ def index(request):
         'exl': EXTERNAL_LINKS,
         'courses': courses,
         'events': events,
+        'time_model_today': time_model_today,
+        'time_model_next': time_model_next,
+        'weekend_days': weekend_days,
     }
     return render(request=request, template_name='core.html', context=context)
