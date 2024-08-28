71a925f0f4311a40455e302438a065dad3117e79
Riffaells <riffaells@gmail.com>
2024-08-28 16:11:54 +0500

    Улучшение модели времени

--- app/news_app/models.py
+++ app/news_app/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from autoslug import AutoSlugField
 from django.utils.translation import gettext as _
 
+
 class News(models.Model):
-    title_ru = models.CharField(max_length=255, verbose_name=_('titleRu'), default='')
-    title_kz = models.CharField(max_length=255, verbose_name=_('titleKz'), default='')
-    title_en = models.CharField(max_length=255, verbose_name=_('titleEn'), default='')
+    title_ru = models.CharField(max_length=255, verbose_name=_('Заголовок Ru'), default='')
+    title_kz = models.CharField(max_length=255, verbose_name=_('Заголовок Kz'), default='')
+    title_en = models.CharField(max_length=255, verbose_name=_('Заголовок En'), default='')
 
     slug = AutoSlugField(populate_from='title_en', unique=True, always_update=True)
-    content_ru = models.TextField(verbose_name='Содержание (рус)', default='')
-    content_kz = models.TextField(verbose_name='Содержание (каз)', default='')
-    content_eng = models.TextField(verbose_name='Содержание (анг)', default='')
+    content_ru = models.TextField(verbose_name='Содержание ru', default='')
+    content_kz = models.TextField(verbose_name='Содержание kz', default='')
+    content_eng = models.TextField(verbose_name='Содержание en', default='')
 
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     main_image = models.ImageField(upload_to='news_images/', verbose_name='Основное изображение', default='')
-    contact_info = models.CharField(max_length=100, blank=True, null=True, verbose_name='Контактная информация', default='')
+    contact_info = models.CharField(max_length=100, blank=True, null=True, verbose_name='Контактная информация',
+                                    default='')
     is_archive = models.BooleanField(default=False)
 
     class Meta:
@@ -32,6 +34,7 @@ class News(models.Model):
             if old_title != self.title_en:
                 self.slug = AutoSlugField(populate_from='title_en').slugify(self.title_en)
         super().save(*args, **kwargs)
+
 
 class NewsImage(models.Model):
     news = models.ForeignKey(News, related_name='additional_images', on_delete=models.CASCADE, default='')
