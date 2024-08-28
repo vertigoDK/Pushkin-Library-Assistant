37c52c9cd31e99f7055bc7e0548409cdeb94a135
Your Name <you@example.com>
2024-08-28 12:10:37 +0500

    добавил апи для последних новостей

--- app/api/services/news_fetcher.py
+++ app/api/services/news_fetcher.py
@@ -1,13 +1,40 @@
+import typing as t
+from django.db.models.query import QuerySet
+from app.news_app.models import News
+
 class NewsFetcher:
     """
-    Класс благодоря которому чат-бот может рассказать о последних 1-5 новостях
+    Класс, благодаря которому чат-бот может рассказать о последних 1-5 новостях.
     """
     
-    def __init__(self):
+    def __init__(self, news_count: int):
         """
         Args:
-            None
+            news_count (int): Количество последних новостей, о которых будет знать нейросеть.
         Returns:
             None
         """
-        pass
+        if 0 < news_count <= 15:
+            self._news_count = news_count
+        else:
+            self._news_count = 5
+    
+    def _get_latest_news(self) -> t.List[t.List[t.Any]]:
+        """
+        Получает последние новости в количестве, заданном при инициализации.
+
+        Returns:
+            QuerySet[News]: QuerySet с последними новостями.
+        """
+        latest_news: QuerySet[News] = News.objects.order_by('-pub_date')[:self._news_count]
+        result: t.List[t.List[t.Any]] = []
+        for news in latest_news:
+            temp_result: t.List[t.Any] = []
+            temp_result.append(news.title_en)
+            temp_result.append(news.content_eng)
+            temp_result.append(news.pub_date)
+            temp_result.append(news.slug)
+
+            result.append(temp_result)
+        
+        return result
--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,8 @@
 # api/urls.py
 from django.urls import path
-from .views import BookSearchAPIView
+from .views import BookSearchAPIView, LatestNewsView
 
 urlpatterns = [
     path('search/', BookSearchAPIView.as_view(), name='book-search'),
+    path('latest-news/', LatestNewsView.as_view(), name='latest-news'),
 ]
