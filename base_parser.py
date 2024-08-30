01d85f4b3e8f1a75cca2076983afe64375997f5c
Denis Kushnaryov <denis.k.kushnaryov@gmail.com>
2024-08-30 12:00:24 +0500

    requirements.txt

---
+++ base_parser.py
@@ -0,0 +1,12 @@
+"""
+Здесь реализован базовый класс для парсеров
+"""
+import requests
+
+class BaseParser:
+    """
+    Базовый класс для парсеров
+    """
+    def __init__(self,
+                 url: str):
+        pass
