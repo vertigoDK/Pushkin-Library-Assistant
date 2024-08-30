356f133186737b89d00f2c8c71f4bc74a9ebf73a
Denis Kushnaryov <denis.k.kushnaryov@gmail.com>
2024-08-30 16:50:07 +0500

    добавил ссылки на anyz и так же удалил base_parser.py

---
+++ links.py
@@ -0,0 +1,17 @@
+# ANYZ LINKS
+
+ANYZ_LEGEND_LINK="https://anyz.pushkinlibrary.kz/ru/iz-ust-v-usta/"
+
+ANYZ_MESTA_SILY="https://anyz.pushkinlibrary.kz/mesta-sily/"
+
+ANYZ_DUKHOVNYE_KORNI="https://anyz.pushkinlibrary.kz/ru/dukhovnye-korni/"
+
+ANYZ_PODKAST="https://anyz.pushkinlibrary.kz/ru/podkast/"
+
+ANYZ_RISOVANNYE_ISTORII="https://anyz.pushkinlibrary.kz/ru/risovannye-istorii/"
+
+ANYZ_GLAZAMI_OCHEVIDCZA="https://anyz.pushkinlibrary.kz/ru/glazami-ochevidcza/"
+
+ANYZ_DOBAV_LEGENDU="https://anyz.pushkinlibrary.kz/ru/dobav-legendu/"
+
+ANYZ_REESTR_OBEKTOV="https://anyz.pushkinlibrary.kz/ru/reestr-obektov-istoriko-kulturnogo-naslediya-vko/"
