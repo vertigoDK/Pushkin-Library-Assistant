98d1ccf6f770344b94959ee3ba8663439c683088
Riffaells <riffaells@gmail.com>
2024-09-02 16:34:17 +0500

    немного не дописан

--- parser/esimder_parser/esimder_parser.py
+++ parser/esimder_parser/esimder_parser.py
@@ -1,8 +1,7 @@
 import asyncio
-from typing import List
-from bs4 import BeautifulSoup
-from requests import Response
+
 import aiohttp
+from bs4 import BeautifulSoup
 
 
 class EsimderParser:
@@ -17,6 +16,10 @@ class EsimderParser:
 
         for link in links:
             lst_name = await self.get_list_name(link[1])
+            for name in lst_name:
+                print(name[0])  # имя
+                text = await self.get_main_text(name[1])
+                # TODO: Дописать формат и все с этим связанное. У меня есть text который просто возврощает описание данного имени
 
         await self.close_session()
 
@@ -27,31 +30,34 @@ class EsimderParser:
         soup = BeautifulSoup(re, 'html.parser')
 
         links = []
-        for i in soup.find('div', {'class': 'offlajn-accordion-140-1-container-inner'}).find_all('a'):
-            links.append((i.text, self.BASE_URL + i.get('href')))
+        for i in soup.find('div', {'class': 'offlajn-accordion-140-1-container-inner'}).find_all('dt',
+                                                                                                 class_='notparent'):
+            link = i.find('a')
+            links.append((link.text, self.BASE_URL + link.get('href')))
 
         s = soup.find('div', class_='display-limit').find('option', {'selected': 'selected'}).text
 
         return links
 
-    async def get_list_name(self, url) -> List[List[str]]:
+    async def get_list_name(self, url: str) -> list[tuple[str, str]]:
         async with self.session.get(f'{url}?limit=0') as response:
             re = await response.text()
+
+        print(url)
 
         soup = BeautifulSoup(re, 'html.parser')
 
         _list_name = soup.find('table', class_='category').find_all('a')
         list_name = list()
         for i in _list_name:
-            list_name.append([i.text, f"{self.BASE_URL}{i.get('href')}"])
-        print(list_name)
+            list_name.append((i.text.replace('\n', '').replace('\r', '').strip(), f"{self.BASE_URL}{i.get('href')}"))
         return list_name
 
-    async def get_main_text(self) -> str:
+    async def get_main_text(self, url: str) -> str:
         """
         :return: возвращает просто текст
         """
-        async with self.session.get('https://esimder.pushkinlibrary.kz/ru/pisateli-i-poety/898-.html') as response:
+        async with self.session.get(url) as response:
             re = await response.text()
         soup = BeautifulSoup(re, 'html.parser')
 
