30998fc896954d2ac1208f0dd32d3ad58f297e9d
Riffaells <riffaells@gmail.com>
2024-09-16 21:03:37 +0500

    фикс тура, фикс книг

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.shortcuts import render
 from django.urls import path, include
-from django.conf.urls import handler404, handler403, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
