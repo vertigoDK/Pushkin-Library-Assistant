e4c634e14feaeef3c93ec392c946036ef42ea636
Denis Kushnaryov <denis.k.kushnaryov@gmail.com>
2024-09-26 12:14:27 +0500

    add intents recognize for books search

--- base/settings.py
+++ base/settings.py
@@ -240,9 +240,6 @@ STATICFILES_FINDERS = (
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
