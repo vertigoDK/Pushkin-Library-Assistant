ce68e8ce117d4d2ab55fcb25de2dc176a1c80cf9
Riffaells <riffaells@gmail.com>
2024-10-15 19:20:34 +0500

    реализация tour

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: QuickStart
-* Template URL: https://bootstrapmade.com/quickstart-bootstrap-startup-website-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
