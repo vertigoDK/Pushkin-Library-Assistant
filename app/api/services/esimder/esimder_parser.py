fcbe4f76c58cb596b071f0a425c6c8e49357b17f
Riffaells <riffaells@gmail.com>
2024-10-20 01:26:33 +0500

    update requirements.txt

--- app/api/services/esimder/esimder_parser.py
+++ app/api/services/esimder/esimder_parser.py
@@ -6,6 +6,9 @@ from typing import List, Any
 
 import aiohttp
 from bs4 import BeautifulSoup
+from unicodedata import category
+
+from flowise.test import url_link
 
 
 class BaseAPIHandler:
@@ -17,42 +20,43 @@ class BaseAPIHandler:
         self.session = None
 
     def get_session(self) -> aiohttp.ClientSession:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
-            "Accept": "text/css,*/*;q=0.1",
-            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Accept-Encoding": "gzip, deflate, br, zstd",
-            "Referer": "https://esimder.pushkinlibrary.kz/ru/",
-            "DNT": "1",
-            "Sec-GPC": "1",
-            "Connection": "keep-alive",
-            "Sec-Fetch-Dest": "style",
-            "Sec-Fetch-Mode": "no-cors",
-            "Sec-Fetch-Site": "same-origin",
-            "Priority": "u=2",
-            "Pragma": "no-cache",
-            "Cache-Control": "no-cache",
-            "TE": "trailers"
-        }
+        if not self.session:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
+                "Accept": "text/css,*/*;q=0.1",
+                "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Referer": "https://esimder.pushkinlibrary.kz/ru/",
+                "DNT": "1",
+                "Sec-GPC": "1",
+                "Connection": "keep-alive",
+                "Sec-Fetch-Dest": "style",
+                "Sec-Fetch-Mode": "no-cors",
+                "Sec-Fetch-Site": "same-origin",
+                "Priority": "u=2",
+                "Pragma": "no-cache",
+                "Cache-Control": "no-cache",
+                "TE": "trailers"
+            }
 
-        cookies = {
-            "2984f25a664ec8dbe4b165aaa42d3810": "4e2d09466fa0c87fdf9a1d25c2b432ac",
-            "61cb0e9afd8a5aec45494fe62d65aa75": "ru-RU"
-        }
+            cookies = {
+                "2984f25a664ec8dbe4b165aaa42d3810": "4e2d09466fa0c87fdf9a1d25c2b432ac",
+                "61cb0e9afd8a5aec45494fe62d65aa75": "ru-RU"
+            }
+            self.session = aiohttp.ClientSession(headers=headers, cookies=cookies)
 
         # Создаем новую сессию с переданными заголовками и куками
-        return aiohttp.ClientSession(headers=headers, cookies=cookies)
+        return self.session
 
     async def get_text(self, url, retries=60):
         attempt = 0
         while attempt < retries:
             attempt += 1
             try:
-                async with self.get_session() as session:
-                    timeout = aiohttp.ClientTimeout(total=1 * attempt)  # Увеличиваем таймаут до 20 секунд
-                    async with session.get(url, timeout=timeout) as response:
-                        text = await response.text()
-                        return text
+                timeout = aiohttp.ClientTimeout(total=1 * attempt)
+                async with self.get_session().get(url, timeout=timeout) as response:
+                    text = await response.text()
+                    return text
             except asyncio.TimeoutError:
                 print(f"Таймаут при попытке запроса: {url}. Попытка {attempt} из {retries}")
                 if attempt == retries:
@@ -75,6 +79,8 @@ class BaseAPIHandler:
         for i in lst.find_all('a'):
             categories.append((i.text, i['href']))
 
+        print(categories)
+
         return categories
 
     async def get_names_in_categories(self, c_url: str) -> List[tuple[str, Any]]:
@@ -88,8 +94,11 @@ class BaseAPIHandler:
         soup = BeautifulSoup(text, "html.parser")
         names: List[tuple[str, Any]] = list()
         lst = soup.find('table', attrs={'class': "category"})
-        for i in lst.find_all('a'):
-            names.append((i.text.strip(), i['href']))
+        try:
+            for i in lst.find_all('a'):
+                names.append((i.text.strip(), i['href']))
+        except:
+            return names
 
         return names
 
@@ -100,21 +109,26 @@ class BaseAPIHandler:
         :param name_url:
         :return:
         """
-        text = await self.get_text(name_url)
+        try:
+            text = await self.get_text(name_url)
 
-        soup = BeautifulSoup(text, 'html.parser')
+            soup = BeautifulSoup(text, 'html.parser')
 
-        soup = soup.find('div', {'class': 'item-page'}).find_all('p')
-        content = ""
-        for p in soup:
-            style = p.get('style', '')
-            if 'text-align: center' in style:
-                # Найден параграф с text-align: center, цикл прерывается.
-                break
-            content += p.get_text()
+            soup = soup.find('div', {'class': 'item-page'}).find_all('p')
+            content = ""
+            for p in soup:
+                style = p.get('style', '')
+                if 'text-align: center' in style or 'margin-bottom: 0cm' in style:
+                    # Найден параграф с text-align: center, цикл прерывается.
+                    break
+                content += p.get_text()
 
-        # Если имена состоят из 2 слов а не из 3
-        full_name = " ".join(content.split()[:len(short_name.split(' '))])
+            content = content.replace('\u00A0', ' ')
+            # Если имена состоят из 2 слов а не из 3
+            full_name = " ".join(content.split()[:len(short_name.split(' '))])
+        except:
+            print(name_url)
+            return
         return full_name, content
 
     async def parse(self):
@@ -124,6 +138,34 @@ class BaseAPIHandler:
         """
 
         categories = await self.get_categories()
+        # categories = [
+        #
+        #     ('Геологи', '/ru/issledovateli-kraya/geologi.html'),
+        #     ('Деятели культуры и искусства', '/ru/deyateli-kulyura-iskusstva.html'),
+        #     ('Библиотеки и музеи', '/ru/deyateli-kulyura-iskusstva/biblioteki-i-muzej.html'),
+        #     ('Изобразительное искусство', '/ru/deyateli-kulyura-iskusstva/izobrazitelnoe-iskusstvo.html'),
+        #     ('Архитекторы', '/ru/deyateli-kulyura-iskusstva/izobrazitelnoe-iskusstvo/arkhitektory.html'), (
+        #         'Декоративно-прикладное искусство',
+        #         '/ru/deyateli-kulyura-iskusstva/izobrazitelnoe-iskusstvo/dekorativno-prikladnoe-iskusstvo.html'),
+        #     ('Художники', '/ru/deyateli-kulyura-iskusstva/izobrazitelnoe-iskusstvo/khudozhniki.html'),
+        #     ('Скульпторы', '/ru/deyateli-kulyura-iskusstva/izobrazitelnoe-iskusstvo/skulptory.html'),
+        #     ('Фотографы', '/ru/deyateli-kulyura-iskusstva/izobrazitelnoe-iskusstvo/fotografy.html'), (
+        #         'Исскуствоведение',
+        #         '/ru/deyateli-kulyura-iskusstva/izobrazitelnoe-iskusstvo/isskustvovedenie.html'),
+        #     ('Кино и ТВ', '/ru/deyateli-kulyura-iskusstva/kino-i-tv.html'),
+        #     ('Режиссеры', '/ru/deyateli-kulyura-iskusstva/kino-i-tv/rezhissery.html'),
+        #     ('Актеры', '/ru/deyateli-kulyura-iskusstva/kino-i-tv/aktery.html'),
+        #     ('Дикторы', '/ru/deyateli-kulyura-iskusstva/kino-i-tv/diktory.html'),
+        #     ('Театр', '/ru/deyateli-kulyura-iskusstva/teatr.html'),
+        #     ('Музыка', '/ru/deyateli-kulyura-iskusstva/muzyka.html'),
+        #     ('Композиторы', '/ru/deyateli-kulyura-iskusstva/muzyka/kompozitory.html'),
+        #     ('Исполнители', '/ru/deyateli-kulyura-iskusstva/muzyka/ispolniteli.html'),
+        #     ('Дирижеры', '/ru/deyateli-kulyura-iskusstva/muzyka/dirizhery.html'),
+        #     ('Хореография', '/ru/deyateli-kulyura-iskusstva/khoreografiya.html'),
+        #     ('Балет', '/ru/deyateli-kulyura-iskusstva/khoreografiya/balet.html'),
+        #     ('Танец', '/ru/deyateli-kulyura-iskusstva/khoreografiya/tanets.html'),
+        #     ('Педагоги', '/ru/pedagogi.html'), ('Герои Социалистического труда', '/ru/social-hero-ru.html'),
+        #     ('Спортсмены', '/ru/sportsmeny.html')]
 
         parse = list()
 
@@ -132,13 +174,15 @@ class BaseAPIHandler:
             names = await self.get_names_in_categories(category_url)
             for short_name, n_url in names:
                 name_url = f"{self.BASE_URL}{n_url}"
+                if name_url.find('#') != -1:
+                    continue
                 full_name, content = await self.get_content_name(short_name, name_url)
                 parse.append({"name_url": name_url, "full_name": full_name, "content": content, "category": category,
                               "category_url": category_url})
 
             self.write(parse)
 
-        await self.close()
+        await self.close_session()
 
     def write(self, data: list):
         file_path = 'esimder_output_list.json'
@@ -163,8 +207,10 @@ class BaseAPIHandler:
         with open(file_path, 'w', encoding='utf-8') as json_file:
             json.dump(existing_data, json_file, ensure_ascii=False, indent=4)
 
-    async def close(self):
-        await self.session.close()
+    async def close_session(self):
+        if self.session is not None:
+            await self.session.close()
+            self.session = None
 
 
 async def main():
@@ -179,76 +225,28 @@ async def main():
     print(execution_time)
 
 
+def read_categories(json_file_path):
+    categories = list()  # Множество для хранения уникальных категорий
+
+    # Чтение JSON данных из файла
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+        # Проход по каждому элементу в данных и добавление категории в множество
+        for entry in data:
+            category = entry.get("category")
+            if category:
+                if len(categories) < 1 or categories[-1] != category:
+                    categories.append(category)
+
+    # Вывод категорий
+    print("Найденные категории:")
+    for category in categories:
+        print(category)
+
+    return categories
+
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-class EsimderParser:
-    BASE_URL = 'https://esimder.pushkinlibrary.kz/index.php'
-
-    def __init__(self):
-        self.session = None
-
-    async def parse(self):
-        self.session = aiohttp.ClientSession()
-        links = await self.get_all_links()
-
-        for link in links:
-            lst_name = await self.get_list_name(link[1])
-            for name in lst_name:
-                print(name[0])  # имя
-                text = await self.get_main_text(name[1])
-                # TODO: Дописать формат и все с этим связанное. У меня есть text который просто возврощает описание данного имени
-
-        await self.close_session()
-
-    async def get_all_links(self):
-        async with self.session.get("https://esimder.pushkinlibrary.kz/ru/ushiteli.html") as response:
-            re = await response.text()
-
-        soup = BeautifulSoup(re, 'html.parser')
-
-        links = []
-        for i in soup.find('div', {'class': 'offlajn-accordion-140-1-container-inner'}).find_all('dt',
-                                                                                                 class_='notparent'):
-            link = i.find('a')
-            links.append((link.text, self.BASE_URL + link.get('href')))
-
-        s = soup.find('div', class_='display-limit').find('option', {'selected': 'selected'}).text
-
-        return links
-
-    async def get_list_name(self, url: str) -> list[tuple[str, str]]:
-        async with self.session.get(f'{url}?limit=0') as response:
-            re = await response.text()
-
-        print(url)
-
-        soup = BeautifulSoup(re, 'html.parser')
-
-        _list_name = soup.find('table', class_='category').find_all('a')
-        list_name = list()
-        for i in _list_name:
-            list_name.append((i.text.replace('\n', '').replace('\r', '').strip(), f"{self.BASE_URL}{i.get('href')}"))
-        return list_name
-
-    async def get_main_text(self, url: str) -> str:
-        """
-        :return: возвращает просто текст
-        """
-        async with self.session.get(url) as response:
-            re = await response.text()
-        soup = BeautifulSoup(re, 'html.parser')
-
-        soup = soup.find('div', {'class': 'item-page'}).find_all('p')
-        text = ""
-        for p in soup:
-            style = p.get('style', '')
-            if 'text-align: center' in style:
-                print("Найден параграф с text-align: center, цикл прерывается.")
-                break
-            text += p.get_text()
-        return text
-
-    async def close_session(self):
-        await self.session.close()
+    # read_categories("esimder_output_list.json")
